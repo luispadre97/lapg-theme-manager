69db947694d4da106fae978b4ef9e2b89dad9507
luispadre123 <119064227+luispadre123@users.noreply.github.com>
2023-04-12 07:52:23 -0600

    Para liverpool

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,120 +1,125 @@
 (function (global) {
-    const ThemeContext = {
-      themes: {},
-      currentTheme: null,
-      styleElement: null,
-      variables: null,
-      loadTheme: function (themeName, theme) {
-        this.themes[themeName] = theme;
-      },
-      setTheme: function (themeName) {
-        if (!themeName) {
-          console.error('No theme name provided.');
-          return;
-        }
-      
-        if (!this.themes[themeName]) {
-          console.error(`Theme '${themeName}' not found.`);
-          return;
-        }
-      
-        this.currentTheme = themeName;
-        this.updateTheme();
-      },
-      updateTheme: function () {
-        if (!this.currentTheme) return;
+  const ThemeContext = {
+    themes: {},
+    currentTheme: null,
+    styleElement: null,
+    variables: null,
+    loadTheme: function (themeName, theme) {
+      this.themes[themeName] = theme;
+    },
+    setTheme: function (themeName) {
+      if (!themeName) {
+        console.error('No theme name provided.');
+        return;
+      }
 
-        const theme = this.themes[this.currentTheme];
-        const css = generateCSSRules(this.currentTheme, theme, this.variables);
-        if (!this.styleElement) {
-          this.styleElement = document.createElement('style');
-          this.styleElement.setAttribute('data-lazy', 'true');
-          document.head.appendChild(this.styleElement);
+      if (!this.themes[themeName]) {
+        console.error(`Theme '${themeName}' not found.`);
+        return;
+      }
+
+      this.currentTheme = themeName;
+      this.updateTheme();
+    },
+    updateTheme: function () {
+      if (!this.currentTheme) return;
+
+      const theme = this.themes[this.currentTheme];
+      const css = generateCSSRules(this.currentTheme, theme, this.variables);
+      if (!this.styleElement) {
+        this.styleElement = document.createElement('style');
+        this.styleElement.setAttribute('data-lazy', 'true');
+        document.head.appendChild(this.styleElement);
+      }
+      this.styleElement.textContent = css;
+      applyStyles();
+    },
+  };
+
+  function generateCSSRules(themeName, theme, variables) {
+    const processStyles = (selector, styles) => {
+      const styleRules = [];
+      const nestedRules = [];
+
+      for (const [property, value] of Object.entries(styles)) {
+        if (typeof value === 'object' && !Array.isArray(value)) {
+          const nestedSelector = property.startsWith('&') ? property.slice(1) : ` ${property}`;
+          nestedRules.push(processStyles(`${selector}${nestedSelector}`, value));
+        } else {
+          const kebabProperty = camelCaseToKebabCase(property);
+          const replacedValue = replaceVariables(value, variables);
+          styleRules.push(`${kebabProperty}: ${replacedValue};`);
         }
-        this.styleElement.textContent = css;
-        applyStyles();
-      },
+      }
+
+      const currentRule = `${selector} {${styleRules.join(' ')}}`;
+      return [currentRule, ...nestedRules].join('\n');
     };
 
-    function generateCSSRules(themeName, theme, variables) {
-      const processStyles = (selector, styles) => {
-        const styleRules = [];
-        const nestedRules = [];
+    let css = '';
 
-        for (const [property, value] of Object.entries(styles)) {
-          if (typeof value === 'object' && !Array.isArray(value)) {
-            const nestedSelector = property.startsWith('&') ? property.slice(1) : ` ${property}`;
-            nestedRules.push(processStyles(`${selector}${nestedSelector}`, value));
-          } else {
-            const kebabProperty = camelCaseToKebabCase(property);
-            const replacedValue = replaceVariables(value, variables);
-            styleRules.push(`${kebabProperty}: ${replacedValue};`);
-          }
-        }
+    for (const [selector, styles] of Object.entries(theme)) {
+      const prefixedSelector = selector.startsWith('.') ? selector : `.${themeName}-${selector}`;
+      css += processStyles(prefixedSelector, styles) + '\n';
+    }
 
-        const currentRule = `${selector} {${styleRules.join(' ')}}`;
-        return [currentRule, ...nestedRules].join('\n');
-      };
+    return css;
+  }
 
-      let css = '';
+  function applyStyles() {
+    const elements = document.querySelectorAll('[data-dynamic-style]');
 
-      for (const [selector, styles] of Object.entries(theme)) {
-        const prefixedSelector = selector.startsWith('.') ? selector : `.${themeName}-${selector}`;
-        css += processStyles(prefixedSelector, styles) + '\n';
+    elements.forEach((element) => {
+      const themeName = element.getAttribute('data-theme') || ThemeContext.currentTheme;
+      const tagName = element.tagName.toLowerCase();
+      const generatedClass = `${themeName}-${tagName}`;
+
+      const customClass = element.getAttribute('class');
+      if (customClass && customClass.split(' ').some(cls => cls.startsWith(themeName))) {
+        return;
       }
 
-      return css;
-    }
-
-    function applyStyles() {
-      const elements = document.querySelectorAll('[data-dynamic-style]');
-
-      elements.forEach((element) => {
-        const themeName = element.getAttribute('data-theme') || ThemeContext.currentTheme;
-        const tagName = element.tagName.toLowerCase();
-        const generatedClass = `${themeName}-${tagName}`;
-
-        const customClass = element.getAttribute('class');
-        if (customClass && customClass.split(' ').some(cls => cls.startsWith(themeName))) {
-          return;
-        }
-
-        element.classList.add(generatedClass);
-      });
-    }
-
-    document.addEventListener('DOMContentLoaded', () => {
-      const initialTheme = document.documentElement.getAttribute('data-theme');
-      ThemeContext.setTheme(initialTheme);
+      element.classList.add(generatedClass);
     });
+  }
 
-    function camelCaseToKebabCase(str) {
-      if (typeof str !== 'string') {
-        throw new TypeError('Input must be a string');
-      }
-      return str.replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, '$1-$2').toLowerCase();
+  document.addEventListener('DOMContentLoaded', () => {
+    const initialTheme = document.documentElement.getAttribute('data-theme');
+    ThemeContext.setTheme(initialTheme);
+  });
+
+  function camelCaseToKebabCase(str) {
+    if (typeof str !== 'string') {
+      throw new TypeError('Input must be a string');
     }
+    return str.replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, '$1-$2').toLowerCase();
+  }
 
-    function replaceVariables(value, variables) {
-      return value.replace(/\$([a-zA-Z0-9]+(-[a-zA-Z0-9]+)?)/g, (_, varName) => variables[varName]);
-    }
-    
+  function replaceVariables(value, variables) {
+    return value.replace(/\$([a-zA-Z0-9]+(-[a-zA-Z0-9]+)?)/g, (_, varName) => variables[varName]);
+  }
 
-    function lapg(options) {
-      return {
-        ThemeContext: ThemeContext,
-        loadThemeFromJSON: (themeName, themeJSON) => ThemeContext.loadTheme(themeName, themeJSON),
-        on: (eventName, callback) => document.addEventListener(`lapg:${eventName}`, (e) => {
-          callback(e.detail);
-        }),
-        emit: (eventName, data) => document.dispatchEvent(new CustomEvent(`lapg:${eventName}`, { detail: data })),
-      };
-    }
 
-    global.lapg = lapg;
-    global.ThemeContext = ThemeContext;
-  })(window);
+  function lapg(options) {
+    return {
+      ThemeContext: ThemeContext,
+      // loadThemeFromJSON: (themeName, themeJSON) => ThemeContext.loadTheme(themeName, themeJSON),
+      // on: (eventName, callback) => document.addEventListener(`lapg:${eventName}`, (e) => {
+      //   callback(e.detail);
+      // }),
+      // emit: (eventName, data) => document.dispatchEvent(new CustomEvent(`lapg:${eventName}`, { detail: data })),
+      loadTheme: function loadTheme(config) {
+        const { loadThemeFromJSON: loadTheme, ThemeContext: { setTheme } } = lapg();
+        loadTheme('multimarcas', { '.marca': config });
+        setTheme('multimarcas');
+      },
+    };
+  }
 
-  var lapg$1 = lapg;
+  global.lapg = lapg;
+  global.ThemeContext = ThemeContext;
+})(window);
+
+var lapg$1 = lapg;
 
 export { lapg$1 as default };
